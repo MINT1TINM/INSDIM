6123107fca3fac55902f0e953ee5f793c9e6617f
MINT <mint@insdim.com>
2019-05-30 23:42:29 +0800

    add type & collaborator

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ for (let index = thisYear; index >= 2014; index--) {
 router.get("/", (req, res) => {
   news.find(
     { visibility: true },
-    "title author content titlePic",
+    "title author type collaborator content titlePic",
     {
       sort: [{ _id: -1 }]
     },
