24e4e7df39cb4d2ec4577053efa1f557e5ffab25
MINT <mint@insdim.com>
2019-07-22 23:12:24 +0800

    locale fix

--- utils/locale.js
+++ utils/locale.js
@@ -1,10 +1,16 @@
+const getCookie = (cname, cookie) => {
+  var name = cname + "=";
+  var ca = cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+};
+
 const getLocale = req => {
-  const cookies = req.headers.cookie;
-  console.log(cookies);
-  let ca = cookies.split("; ");
-  let name = "locale=";
-  let full = ca[1].length;
-  let locale = ca[1].substring(name.length, full);
+  const cookie = req.headers.cookie;
+  const locale = getCookie("locale", cookie);
   return locale;
 };
 export { getLocale };
