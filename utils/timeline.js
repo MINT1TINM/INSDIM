e5f6ccb0ab3df52d1f7e2afdb1f61bdf6cb11215
MINT <mint@insdim.com>
2019-07-23 12:32:00 +0800

    timeline

--- utils/timeline.js
+++ utils/timeline.js
@@ -8,44 +8,53 @@ const chiefDesigner = [
   {},
   {
     time: "2014/08/12",
-    content: "成立INSLENS (INSDIM曾用名)"
+    content: "成立INSLENS (INSDIM曾用名)",
+    enContent: "found INSLENS"
   },
   {
     time: "2016/10/06",
-    content: "INSDIM 正式成立"
+    content: "INSDIM 正式成立",
+    enContent: "found INSDIM (originally named INSLENS)"
   },
   {
     time: "2017/02/14",
-    content: "发布自有品牌HUMASIM"
+    content: "发布自有品牌HUMASIM",
+    enContent: "introducing individual brand HUMASIM"
   },
   {
     time: "2019/01/25",
-    content: "成立DIMLAB数据分析实验室"
+    content: "成立DIMLAB数据分析实验室",
+    enContent: "found DIMLAB - a data analysis laboratory"
   },
   {},
   {},
   {
     time: "2019/04/20",
-    content: "获得上海市优秀毕业作品"
+    content: "获得上海市优秀毕业作品",
+    enContent: "best diploma awards of shanghai"
   }
 ];
 
 const awards = [
   {
     time: "2018/02/15",
-    content: "GREENWOOD入围「红点设计奖」"
+    content: "GREENWOOD入围「红点设计奖」",
+    enContent: "greenwood - reddot design award - nominated"
   },
   {
     time: "2019/05/14",
-    content: "BLOOM获得「汇创青春」一等奖 (TOP 15)"
+    content: "BLOOM获得「汇创青春」一等奖 (TOP 15)",
+    enContent: "bloom - think youth - first prize (top 15)"
   },
   {
     time: "2019/05/14",
-    content: "WAISTLINE获得「汇创青春」一等奖"
+    content: "WAISTLINE获得「汇创青春」一等奖",
+    enContent: "waistline - think youth - first prize"
   },
   {
     time: "2019/05/14",
-    content: "DRAWER获得「汇创青春」三等奖"
+    content: "DRAWER获得「汇创青春」三等奖",
+    enContent: "drawer - think youth - third prize"
   }
 ];
 
@@ -53,19 +62,22 @@ const exhibition = [
   {
     time: "2016/11",
     content: "「ABOUT SET」坐具专题展",
-    enContent: "xxx"
+    enContent: "「 about set 」 exhibition"
   },
   {
     time: "2017/02",
-    content: "「ANILAMP COLLECTION」灯具专题展"
+    content: "「ANILAMP COLLECTION」灯具专题展",
+    enContent: "「 anilamp collection 」 exhibition"
   },
   {
     time: "2018/07",
-    content: "「FUTURE MATERIAL」新材料展"
+    content: "「FUTURE MATERIAL」新材料展",
+    enContent: "「 future material 」 exhibition"
   },
   {
     time: "2019/05",
-    content: "「汇创青春」优秀作品展"
+    content: "「汇创青春」优秀作品展",
+    enContent: "「 think youth 」 best of 2019 exhibition"
   }
 ];
 
